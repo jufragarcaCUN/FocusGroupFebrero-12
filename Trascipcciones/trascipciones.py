a6ff9260aa8cb5af57f721d73064ab5b44e6ae56
jufragarcaCUN <juan_garnicac@cun.edu.co>
2026-04-13 11:48:52 -0500

    Agrega dashboards de todas las sedes

---
+++ Trascipcciones/trascipciones.py
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ffe567aa",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "C:\\Users\\juan_garnicac\\AppData\\Local\\Temp\\ipykernel_23292\\2197916885.py:8: FutureWarning: \n",
+      "\n",
+      "All support for the `google.generativeai` package has ended. It will no longer be receiving \n",
+      "updates or bug fixes. Please switch to the `google.genai` package as soon as possible.\n",
+      "See README for more details:\n",
+      "\n",
+      "https://github.com/google-gemini/deprecated-generative-ai-python/blob/main/README.md\n",
+      "\n",
+      "  import google.generativeai as genai\n",
+      "usage: ipykernel_launcher.py [-h] [-o SALIDA] audio\n",
+      "ipykernel_launcher.py: error: the following arguments are required: audio\n"
+     ]
+    },
+    {
+     "ename": "SystemExit",
+     "evalue": "2",
+     "output_type": "error",
+     "traceback": [
+      "An exception has occurred, use %tb to see the full traceback.\n",
+      "\u001b[31mSystemExit\u001b[39m\u001b[31m:\u001b[39m 2\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "C:\\Users\\juan_garnicac\\AppData\\Roaming\\Python\\Python313\\site-packages\\IPython\\core\\interactiveshell.py:3707: UserWarning: To exit: use 'exit', 'quit', or Ctrl-D.\n",
+      "  warn(\"To exit: use 'exit', 'quit', or Ctrl-D.\", stacklevel=1)\n"
+     ]
+    }
+   ],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
